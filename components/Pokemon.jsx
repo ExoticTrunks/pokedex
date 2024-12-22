4debd87a33d1dba85d5f7759f0bde8750aa43c35
Rafael <ralipalermo@hotmail.com>
2024-12-22 23:29:46 +0800

    fix image error

--- components/Pokemon.jsx
+++ components/Pokemon.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {motion} from 'framer-motion'
-
+import Image from 'next/image';
 
 
 const Pokemon = ({pokeName, pokeID, pokeImg, pokeType}) => {
@@ -12,7 +12,7 @@ const Pokemon = ({pokeName, pokeID, pokeImg, pokeType}) => {
     whileTap={{ scale: 0.9 }}
     className='bg-white rounded-lg shadow-lg p-4' key={pokeID}>
     <motion.div className='flex flex-col items-center'>
-                <img src={pokeImg} alt={pokeName} className='w-32 h-32'/>
+                <Image src={pokeImg} alt={pokeName} className='w-44 h-44 p-5 shadow rounded border-none md:w-64 md:h-64 '/>
                 <p className='capitalize mt-3'>{pokeName}</p>
                 <div className='flex space-x-3 pt-2'>
                   {pokeType.map((type) => (
